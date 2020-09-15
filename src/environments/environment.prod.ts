4ddba5ee7c8740b50eecb20c856e78a9563cbd67
HimanshuUpreti <hiteshupreti1997@gmail.com>
2020-09-15 22:43:27 +0530

    2irst commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA8yFxRIWD2au5_bprPHQUdANAY5gRej9Y',
+      authDomain: 'alien-walker-198610.firebaseapp.com',
+      databaseURL: 'https://alien-walker-198610.firebaseio.com',
+      projectId: 'alien-walker-198610',
+      storageBucket: 'alien-walker-198610.appspot.com',
+      messagingSenderId: '246443202084',
+      appId: '1:246443202084:web:b47339cd7a713f10936ab1',
+      measurementId: 'G-FFRLWKNMFK'
+    }
 };
